62edb2483476c8ed615510334acbd2f22d837eda
Mark Suson <mark.suson@gmail.com>
2023-12-29 12:54:30 -0500

    changing style

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      backgroundImage: (theme) => ({
+        "gradient-to-b": "linear-gradient(to bottom, var(--tw-gradient-stops))",
+      }),
       colors: {
         Crimson: "hsl(0deg, 100%, 20%)",
         Dark_Slate_Grey: "hsl(234, 29%, 20%)",
@@ -15,6 +18,17 @@ export default {
       fontFamily: {
         Outfit: ["Outfit", "sans-serif"],
       },
+      borderWidth: {
+        hairline: "0.5px",
+      },
+      boxShadow: {
+        dark: "0 0 15px -3px rgba(87, 91, 93, 0.7), 0 4px 6px -2px rgba(0, 0, 0, 0.05)",
+      },
+    },
+  },
+  variants: {
+    extend: {
+      backgroundImage: ["responsive", "hover", "focus", "active"],
     },
   },
   plugins: [],
